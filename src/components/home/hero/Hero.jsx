9c9e135d677eee3ca163e448773b31542b9c2258
AshekurRahman <31946194+AshekurRahman@users.noreply.github.com>
2023-03-13 13:56:12 +0600

    Update 1

---
+++ src/components/home/hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <section className='hero'>
+      <div className="container">
+        <h1>
+          <label>Over <span>6,500</span>Curated Design</label>
+          <br />
+          <label>Resource, <span>Graphic & Website</span> Templates </label>
+        </h1>
+      </div>
+    </section>
+  )
+}
---
+++ src/components/pages/Pages.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {Header} from '../common/Header';
+import {Footer} from '../common/Footer';
+import {Home} from '../home/Home';
+
+
+export const Pages = () => {
+  return (
+    <>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+        </Switch>
+        <Footer />
+      </Router>      
+    </>
+  )
+}
